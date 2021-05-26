b915dbff0541498bf161a3ad880a864c0d337dff
Jorge <chauxjorge@hotmail.com>
2021-05-25 20:23:23 -0500

    [Add] authentication and users routes

---
+++ api/v1/views/authentication.py
@@ -0,0 +1,41 @@
+from flask import json, jsonify, request
+from app import mysql
+from api.v1.views import app_views
+import jwt
+
+
+@app_views.route('/signup/', methods=['POST'])
+def signup():
+
+    args = request.get_json()
+    
+    name = args.get("name")
+    lastname = args.get("lastname")
+    govid = args.get("govid")
+    email = args.get("email")
+    company = args.get("company")
+    password = args.get("password")
+    # cursor allows to execute queries in mysql 
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO users (name, lastname, govid, email, company, password) VALUES (%s, %s, %s, %s, %s, %s)', (name, lastname, govid, email, company, password))
+
+    mysql.connection.commit()
+    return jsonify("REGISTERED SUCCESFULLY")
+
+@app_views.route('/login/', methods=['POST'])
+def login():
+
+    args = request.get_json()
+
+    useremail = args.get("email")
+    password = args.get("password")
+    # cursor allows to execute queries in mysql 
+    cur = mysql.connection.cursor()
+
+    exist = cur.execute('SELECT * FROM users WHERE email = %s AND password = %s;', (useremail, password))
+
+    if (exist == 1):
+        encoded_jwt = jwt.encode({"email": useremail}, "secret", algorithm="HS256")
+        return jsonify({"TOKEN:": encoded_jwt})  
+
+    return jsonify("NO EXIST")
---
+++ api/v1/views/users.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, redirect, url_for, flash, jsonify
+from app import mysql
+from api.v1.views import app_views
+
+
+# app_views variable created in api.v1.views __init.py__
+# app_views value is api/v1 
+@app_views.route('/users/all')
+def get_all_users():
+    cur = mysql.connection.cursor()
+    query = 'SELECT * FROM users'
+    cur.execute(query)
+    #data receives a tuple of tuple
+    data = cur.fetchall()
+    return jsonify(data)
+
