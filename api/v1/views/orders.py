94de1047de6d08992219977b92d04e489cec801e
Jorge <chauxjorge@hotmail.com>
2021-05-27 16:46:04 -0500

    [Add] validate token in every route at orders.py and users.py

--- api/v1/views/orders.py
+++ api/v1/views/orders.py
@@ -1,59 +1,91 @@
-from flask import jsonify
+from flask import jsonify, request
 from app import mysql
 from api.v1.views import app_views
+import jwt
+
+
+def valid_token(encoded_jwt):
+    try:
+        payload = jwt.decode(encoded_jwt, "secret", algorithms=["HS256"])
+        user_email = payload.get('email')
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM users WHERE email = %s;', [user_email])
+        data = cur.fetchall()
+        if (data):
+            return True
+    except:
+        return False
 
 # app_views variable created in api.v1.views __init.py__
 # app_views value is api/v1 
 @app_views.route('/orders/<id>')
 def get_order_by_id(id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM orders WHERE orderid = %s;', [id])
-    #data receives a tuple of tuple
-    data = cur.fetchall()
-    return jsonify(data)
-    """
-    return jsonify({
-        "orderid": data[0][0],
-        "date": data[0][1],
-        "total": data[0][2],
-        "subtotal": data[0][3],
-        "taxes": data[0][4],
-        "paid": data[0][5]
-    })
-    """
+
+    token = request.headers.get('x-auth-token')
+    isValid = valid_token(token)
+
+    if(isValid):
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM orders WHERE orderid = %s;', [id])
+        #data receives a tuple of tuple
+        data = cur.fetchall()
+        return jsonify(data)
+        """
+        return jsonify({
+            "orderid": data[0][0],
+            "date": data[0][1],
+            "total": data[0][2],
+            "subtotal": data[0][3],
+            "taxes": data[0][4],
+            "paid": data[0][5]
+        })
+        """
+    return jsonify("INVALID TOKEN")
 
 # receive string with order ids separated by coma and return all orders
 @app_views.route('/orders/ordersid/<orders_id>')
 def get_orders_by_orderid(orders_id):
-    cur = mysql.connection.cursor()
-    strIds = orders_id
-    arrIds = []
-    twoDigitid = ""
-    count = 0
 
-    for c in strIds:
-        if c is ",":
+    token = request.headers.get('x-auth-token')
+    isValid = valid_token(token)
+
+    if(isValid):
+        cur = mysql.connection.cursor()
+        strIds = orders_id
+        arrIds = []
+        twoDigitid = ""
+        count = 0
+
+        for c in strIds:
+            if c is ",":
+                count = count + 1
+                cur.execute('SELECT * FROM orders WHERE orderid = %s;', [twoDigitid])
+                #data receives a tuple of tuple
+                data = cur.fetchall()
+                arrIds.append(data[0])
+                twoDigitid = ""
+                continue
             count = count + 1
-            cur.execute('SELECT * FROM orders WHERE orderid = %s;', [twoDigitid])
-            #data receives a tuple of tuple
-            data = cur.fetchall()
-            arrIds.append(data[0])
-            twoDigitid = ""
-            continue
-        count = count + 1
-        twoDigitid = twoDigitid + c
-        if(count == len(strIds)):
-            cur.execute('SELECT * FROM orders WHERE orderid = %s;', [twoDigitid])
-            #data receives a tuple of tuple
-            data = cur.fetchall()
-            arrIds.append(data[0])
-    return jsonify(arrIds)
+            twoDigitid = twoDigitid + c
+            if(count == len(strIds)):
+                cur.execute('SELECT * FROM orders WHERE orderid = %s;', [twoDigitid])
+                #data receives a tuple of tuple
+                data = cur.fetchall()
+                arrIds.append(data[0])
+        return jsonify(arrIds)
+    return jsonify("INVALID TOKEN")
 
 # Get all orders by userId
 @app_views.route('/orders/userid/<user_id>')
 def get_order_by_userid(user_id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM orders WHERE userid = %s;', [user_id])
-    #data receives a tuple of tuple
-    data = cur.fetchall()
-    return jsonify(data)
+
+    token = request.headers.get('x-auth-token')
+    isValid = valid_token(token)
+
+    if(isValid):
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM orders WHERE userid = %s;', [user_id])
+        #data receives a tuple of tuple
+        data = cur.fetchall()
+        return jsonify(data)
+    return jsonify("INVALID TOKEN")
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,15 +1,34 @@
-from flask import jsonify
+from flask import jsonify, request
 from app import mysql
 from api.v1.views import app_views
+import jwt
 
+
+def valid_token(encoded_jwt):
+    try:
+        payload = jwt.decode(encoded_jwt, "secret", algorithms=["HS256"])
+        user_email = payload.get('email')
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM users WHERE email = %s;', [user_email])
+        data = cur.fetchall()
+        if (data):
+            return True
+    except:
+        return False
 
 # app_views variable created in api.v1.views __init.py__
 # app_views value is api/v1 
 @app_views.route('/users/all')
 def get_all_users():
-    cur = mysql.connection.cursor()
-    query = 'SELECT * FROM users'
-    cur.execute(query)
-    #data receives a tuple of tuple
-    data = cur.fetchall()
-    return jsonify(data)
+
+    token = request.headers.get('x-auth-token')
+    isValid = valid_token(token)
+
+    if (isValid):
+        cur = mysql.connection.cursor()
+        query = 'SELECT * FROM users'
+        cur.execute(query)
+        #data receives a tuple of tuple
+        data = cur.fetchall()
+        return jsonify(data)
+    return jsonify("INVALID TOKEN")
