e1b07d0de57a1593d517793f2c61a9a335972f5f
Jorge <chauxjorge@hotmail.com>
2021-06-03 18:02:06 -0500

    Implement orders query options in home html and py

--- frontend/routes/home.py
+++ frontend/routes/home.py
@@ -4,5 +4,17 @@ import requests
 
 @routes.route('/home', methods=['POST', 'GET'])
 def home():
-    
-    return render_template('home.html')
+    if (request.method == 'POST'):
+        optionSelected = request.form['queryOption']
+        if optionSelected == 'allUsers':
+            return redirect('/users')
+        elif optionSelected == 'orderById':
+            #orderId es el name en el form de home.html
+            order_id = request.form['orderId']
+            return redirect('/order/{}'.format(order_id))
+        elif optionSelected == 'ordersById':
+            orders = request.form['ordersByComma']
+            return redirect('/orders/ordersid/{}'.format(orders))
+        elif optionSelected == 'userOrders':
+            user_id = request.form['userId']
+            return redirect('/orders/userid/{}'.format(user_id))
